5fd3d6f3ac4dcdf7f99bc6880af7b46b29985150
‪Alaaddin yahia <150624261+ALAAEDDIN97120@users.noreply.github.com>
2025-05-05 15:49:08 +0300

    feat(GeographChart):add GeographCahrt UI component

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
+import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -35,8 +35,8 @@ function App() {
 							<Route path="/calendar" element={<Calendar />} />
 							<Route path="/bar" element={<Bar />} />
 							<Route path="/pie" element={<Pie />} />
-							{/* <Route path="/line" element={<Line />} /> */}
-							{/* <Route path="/geography" element={<Geography />} /> */}
+							<Route path="/line" element={<Line />} />
+							<Route path="/geography" element={<Geography />} />
 						</Routes>
 					</main>
 				</div>
